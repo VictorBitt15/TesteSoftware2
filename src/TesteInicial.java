22f4cb2b2944fd8da5922cf44c39946c272ba67b
victor.bittencourt.075 <victor.bittencourt.075@imd.local>
2022-10-19 16:27:12 -0300

    Att

--- src/TesteInicial.java
+++ src/TesteInicial.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,7 +19,7 @@ public class TesteInicial {
 	public void setUp() throws Exception{
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
-		baseURL = "C:\\Users\\victor.bittencourt.075\\Downloads\\pagina.html";
+		baseURL = "C:\\Users\\victor.bittencourt.075\\Downloads\\paginamod.html";
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
@@ -32,8 +33,47 @@ public class TesteInicial {
 	public void testCriar() throws Exception{
 		driver.get(baseURL);
 		driver.findElement(By.className("btn")).click();
-		assertTrue(driver.findElement(By.id("exampleInputEmail1")).getText().contains("Erro"));
+		assertTrue(!driver.findElement(By.id("email1")).getAttribute("validationMessage").isEmpty());
 	}
+	@Test
+	public void testPreencherCorreto() throws Exception{
+		try {
+			driver.get(baseURL);
+			driver.findElement(By.id("email1")).sendKeys("fulano@outlook.com");
+			driver.findElement(By.id("psw1")).sendKeys("123456");
+			driver.findElement(By.className("btn")).click();
+			assertTrue(!driver.switchTo().alert().getText().isBlank());
+			driver.switchTo().alert().accept();
+		
+		} catch (UnhandledAlertException e) {
+		
+			}
+	}
+	@Test
+	public void testPreencherIncorreto() throws Exception{
+		try {
+			driver.get(baseURL);
+			driver.findElement(By.id("email1")).sendKeys("fulanooutlook.com");
+			driver.findElement(By.id("psw1")).sendKeys("123456");
+			driver.findElement(By.className("btn")).click();
+			assertTrue(!driver.findElement(By.id("email1")).getAttribute("validationMessage").isEmpty());
+		
+		} catch (UnhandledAlertException e) {
+			
+		}
+	}
+	
+	@Test
+	public void testChecked() throws Exception{
+		driver.get(baseURL);
+		driver.findElement(By.className("checkbox")).findElement(By.tagName("INPUT")).click();
+		driver.findElement(By.id("optionsCheckbox2")).click();
+		driver.findElement(By.id("optionsRadios2")).click();
+		driver.findElement(By.className("btn")).click();
+		assertTrue(!driver.findElement(By.id("email1")).getAttribute("validationMessage").isEmpty());
+
+	}
+	
 	@After
 	public void tearDown() {
 		driver.close();
